fd81e2ace590e2f927f8dc403576685aab4b9ae1
BoozeMoose <dave.nussbaumer@gmail.com>
2017-04-26 10:59:18 +0200

    initial

---
+++ src/components/Watchlist.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="watchlist">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'watchlist',
+  data () {
+    return {
+      msg: 'Watchlist Component'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/components/Home'
+import Watchlist from '@/components/Watchlist'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/watchlist',
+      name: 'watchlist',
+      component: Watchlist
+    }
+  ]
+})
